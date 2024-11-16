a7152b5548c42880e3d689a2d454fbe93f9fbab1
Jake Bulman <jakebulman@hotmail.com>
2024-11-16 17:40:39 +0000

    Settings updates for S3 storage

--- gigma/gigma/settings.py
+++ gigma/gigma/settings.py
@@ -229,7 +229,15 @@ else:
     AWS_S3_VERITY = True
     AWS_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STORAGES = {
+        "default": {
+            "BACKEND" : "storages.backends.s3boto3.S3StaticStorage",
+        },
+
+        "staticfiles":  {
+            "BACKEND" : "storages.backends.s3boto3.S3StaticStorage",
+        },
+    }
     STATIC_ROOT = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
