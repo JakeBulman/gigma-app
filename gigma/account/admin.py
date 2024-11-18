2954ae2d56a00b0ac47a7aa5bb244a89999da596
Jake Bulman <jakebulman@hotmail.com>
2024-11-18 17:49:54 +0000

    Discipline admin and picture chooser

--- gigma/account/admin.py
+++ gigma/account/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
-from django.conf import settings
-from django.conf.urls.static import static
+from account.models import Discipline
 
-urlpatterns = [
-    path('', include('main.urls')),
-    path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
-    path('events/', include('events.urls')),
-]
+# Register your models here.
+class DisciplineAdmin(admin.ModelAdmin):
+    pass
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+admin.site.register(Discipline)
